1ecb8c60c4077f3515d1dadfad12aeeef12a5afa
ueokaj <nimzo.i.d.6689@gmail.com>
2017-03-25 21:04:01 +0900

    Add namedQuery and Criteria API samples.

--- src/main/java/com/qiita/nimzo6689/etude/jpa/dao/UsersDao.java
+++ src/main/java/com/qiita/nimzo6689/etude/jpa/dao/UsersDao.java
@@ -1,7 +1,13 @@
 package com.qiita.nimzo6689.etude.jpa.dao;
 
+import com.qiita.nimzo6689.etude.jpa.code.AuthorityType;
 import com.qiita.nimzo6689.etude.jpa.entity.Users;
+import com.qiita.nimzo6689.etude.jpa.entity.Users_;
 import java.util.List;
+import javax.persistence.Tuple;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import lombok.extern.log4j.Log4j2;
 
 /**
@@ -11,23 +17,53 @@ import lombok.extern.log4j.Log4j2;
  */
 @Log4j2
 public class UsersDao extends AbstractDao {
-
+    
     private static final long serialVersionUID = 3208415200806376976L;
-
+    
     public static void main(String[] args) {
         UsersDao usersDao = new UsersDao();
-        usersDao.method("br").stream().forEach(log::info);
+        log.info(usersDao.findByLoginId("br"));
+        
+        usersDao.findAll().stream().forEach(log::info);
+        usersDao.findByAuthorityType(AuthorityType.NORMAL).stream()
+                .map(tuple -> "{" + tuple.get(0) + ", " + tuple.get(1) + "}")
+                .forEach(log::info);
     }
-
-    public List<Users> method(String loginId) {
+    
+    public Users findByLoginId(String loginId) {
         try {
             String sql = "SELECT u FROM Users u WHERE u.loginId = :loginId";
             return entityManager.createQuery(sql, Users.class)
                     .setParameter("loginId", loginId)
+                    .getSingleResult();
+        } finally {
+            entityManager.clear();
+        }
+    }
+    
+    public List<Users> findAll() {
+        try {
+            return entityManager.createNamedQuery("Users.findAll", Users.class)
                     .getResultList();
         } finally {
             entityManager.clear();
         }
     }
-
+    
+    public List<Tuple> findByAuthorityType(AuthorityType authorityType) {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Tuple> query = criteriaBuilder.createTupleQuery();
+        
+        Root<Users> root = query.from(Users.class);
+        
+        query.select(criteriaBuilder.tuple(
+                root.get(Users_.userName), root.get(Users_.authorityType)))
+                .where(
+                        criteriaBuilder.equal(root.get(Users_.authorityType), authorityType),
+                        criteriaBuilder.equal(root.get(Users_.isDeleted), false)
+                ).orderBy(criteriaBuilder.asc(root.get(Users_.createdAt)));
+        return entityManager.createQuery(query)
+                .getResultList();
+    }
+    
 }
